2a42a5c9cb1fcdf794510bd8e93e9a8e539fb46f
jahangirpial <jahangiralompial@gmail.com>
2020-05-31 21:04:41 +0600

    File case error fix

--- application/controllers/Superadmin.php
+++ application/controllers/Superadmin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class SuperAdmin extends CI_Controller {
+class Superadmin extends CI_Controller {
 
 	public function __construct()
 	{
